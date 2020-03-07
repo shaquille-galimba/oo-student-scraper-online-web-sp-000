55851192c44be298f8547218c07c7a627b929571
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-07 07:47:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,13 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     index_page = Nokogiri::HTML(html)
+    verified_students = []
+    index_page.css("div.student-card").each do |student|
+      hash = {
+        name : student.css("h4.student-name").text
+        location : student.css("p.student-location").text
+        profile_url : student.css("a").attribute("href").value
+      }
     binding.pry
   end
 
@@ -18,4 +25,4 @@ end
 # roster_container : index_page.css("div.student-card")
 # name : roster_container.css("h4.student-name").text
 # location : roster_container.css("p.student-location").text
-index_page.css("div.student-card").first.css("a").attribute("href").value
+# profile_url : index_page.css("div.student-card").first.css("a").attribute("href").value
