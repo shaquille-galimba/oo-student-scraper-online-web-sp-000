5faf87049555de5c1af7b2c68e670c4ef2e774b8
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-07 07:10:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,5 +15,5 @@ class Scraper
 
 end
 
-index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
-Scraper.scrape_index_page(index_url)
+# index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
+# Scraper.scrape_index_page(index_url)
