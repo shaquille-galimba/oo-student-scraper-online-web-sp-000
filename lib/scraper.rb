5929dc5a293932b4d8aa3685cbe300821108161c
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-07 08:01:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,14 +18,15 @@ class Scraper
       verified_students << hash
     end
     verified_students
-    # binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     profile_page = Nokogiri::HTML(html)
+    binding.pry
 
     student_attribute = {}
+    
   end
 
 end
