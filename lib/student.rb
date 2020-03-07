2abf791ddb8bd5cc3997af955e19a40b6d9f5c45
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-07 08:55:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    # students_array.each {|student_attribute| self.new}
   end
 
   def add_student_attributes(attributes_hash)
