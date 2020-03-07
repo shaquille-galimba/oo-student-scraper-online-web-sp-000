b2deb142266db076e55cbd5dcae95cbc92c83e76
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-07 08:39:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,6 +27,21 @@ class Scraper
 
     student_attribute = {}
 
+    profile_page.css("div.social-icon-container a").each do |social|
+      if social.attribute("href").value.include?("twitter")
+        student_attribute[:twitter] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("linkedin")
+        student_attribute[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?(github)
+        student_attribute[:github] = social.attribute("href").value
+      else
+        student_attribute[:blog] = social.attribute("href").value
+      end
+    end
+
+    student_attribute[:profile_quote] = profile_page.css("div.profile-quote").text
+    student_attribute[:bio] = profile_page.css("div.description-holder p").text
+    student_attribute
   end
 
 end
