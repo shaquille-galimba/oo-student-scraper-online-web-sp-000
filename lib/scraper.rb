5cb14d4e0319e1cc39cb162dc1940f2ca89b91c3
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-07 07:04:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = File.read(index_url)
     index_page = Nokogiri::HTML(html)
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
